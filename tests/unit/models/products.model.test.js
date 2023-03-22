64181c5574faeec22604a5e0860071cc50101c4d
calebeLAR <ca6ele@gmail.com>
2023-03-22 15:48:16 -0400

    testa getAllProductsFromDatabase em caso de sucesso

---
+++ tests/unit/models/products.model.test.js
@@ -0,0 +1,19 @@
+const sinon = require('sinon');
+const { exect } = require('chai');
+const connection = require('../../../src/models/connection');
+const mockProducts = require('./moks/products.model.mock');
+const productsModel = require('../../../src/models/products.model');
+
+describe('testes unitários para a camada model de produtos.', async function () {
+  it('testa o retorno da função getAllProductsFromDatabase em caso de sucesso.', async function () {
+    // Arrange
+    sinon.stub(connection, 'execute').resolves(mockProducts.allProducts);
+    // Act
+    const sucessResponse = productsModel.getAllProductsFromDatabase();
+    console.log(sucessResponse);
+    // Assert
+
+    
+    sinon.restore();
+  });
+});
