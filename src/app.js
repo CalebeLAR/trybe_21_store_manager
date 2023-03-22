7261fdbb09e6dab9ba102c963b1e21f81feee96b
calebeLAR <ca6ele@gmail.com>
2023-03-22 15:21:04 -0400

    req01: cria endpont get para testes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const { procuctsModel } = require('./models');
 
 const app = express();
+app.use(express.json());
 
 // não remova esse endpoint, é para o avaliador funcionar!
 app.get('/', (_request, response) => {
   response.send();
+});
+
+app.get('/products', async (req, res) => {
+  const allProducts = await procuctsModel.getAllProductsFromDatabase();
+  res.status(200).json(allProducts);
+});
+
+app.get('/products/1', (req, res) => {
+  res.status(200).json('/products/1 OK');
 });
 
 // não remova essa exportação, é para o avaliador funcionar
