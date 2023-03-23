23c721d00017ee2d0209de6e273856c3f4372953
calebeLAR <ca6ele@gmail.com>
2023-03-23 14:48:57 -0400

    tests asserts da model

--- tests/unit/models/products.model.test.js
+++ tests/unit/models/products.model.test.js
@@ -1,5 +1,5 @@
 const sinon = require('sinon');
-const { exect } = require('chai');
+const { expect } = require('chai');
 const connection = require('../../../src/models/connection');
 const mockProducts = require('./moks/products.model.mock');
 const productsModel = require('../../../src/models/products.model');
@@ -7,24 +7,31 @@ const productsModel = require('../../../src/models/products.model');
 describe('testes unitários para a camada model de produtos.', async function () {
   it('testa o retorno da função getAllProductsFromDatabase em caso de sucesso.', async function () {
     // Arrange
-    sinon.stub(connection, 'execute').resolves(mockProducts.allProducts);
-    // Act
-    const sucessResponse = productsModel.getAllProductsFromDatabase();
-    console.log(sucessResponse);
-    // Assert
+    const { allProducts } = mockProducts;
+    const sucessRepostse = allProducts[0];
+    sinon.stub(connection, 'execute').resolves(allProducts);
 
-    
+    // Act
+    const response = await productsModel.getAllProductsFromDatabase();
+
+    // Assert
+    expect(response).to.be.deep.equal(allProducts[0]);
+
     sinon.restore();
   });
 
   it('testa o retorno da função getProductByIdFromDatabase em caso de sucesso.', async function () {
     // Arrange
-    sinon.stub(connection, 'execute').resolves(mockProducts.allProducts);
-    // Act
-    const sucessResponse = productsModel.getProductByIdFromDatabase();
-    console.log(sucessResponse);
-    // Assert
+    const { allProducts } = mockProducts;
+    const sucessResponse = allProducts[0][0] // primeiro produto.
+    const productID = 1;
+    sinon.stub(connection, 'execute').resolves(allProducts);
 
+    // Act
+    const response = await productsModel.getProductByIdFromDatabase(productID);
+
+    // Assert
+    expect(response).to.be.deep.equal(sucessResponse);
 
     sinon.restore();
   });
