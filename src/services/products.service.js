9e43dd273003814d016096a4e8a8ef027d63ed13
calebeLAR <ca6ele@gmail.com>
2023-03-24 10:43:44 -0400

    req01: cria camada service e funçoes requestAllProducts e requestProductById

--- src/services/products.service.js
+++ src/services/products.service.js
@@ -1,11 +1,23 @@
 const { procuctsModel } = require('../models');
+const { valideteProductId } = require('./validations/productsValidations');
 
 const requestAllProducts = async () => {
   const allProducts = await procuctsModel.getAllProductsFromDatabase();
-  
+
   return { type: null, message: allProducts };
+};
+
+const requestProductById = async (productID) => {
+  const error = await valideteProductId(productID);
+  if (error) return { type: 'INPUT_VALUE', message: error.message };
+
+  const product = await procuctsModel.getProductByIdFromDatabase(productID);
+  if (!product) return { type: 'PRODUCT_NOT_FOUND', message: 'Product not found' };
+
+  return { type: null, message: product };
 };
 
 module.exports = {
   requestAllProducts,
+  requestProductById,
 };
