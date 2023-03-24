aee1ea68af57d7d9b51b8ca9b74423c76a1ec8c2
calebeLAR <ca6ele@gmail.com>
2023-03-24 14:48:29 -0400

    testes para camada model

---
+++ tests/unit/models/mocks/products.model.mock.js
@@ -0,0 +1,14 @@
+const allProducts = [
+  [
+    {"id": 1, "name": "Martelo de Thor" },
+    { "id": 2, "name": "Traje de encolhimento" },
+    { "id": 3, "name": "Escudo do Capitão América" }
+  ],
+  [
+  undefined,
+  ]
+]
+
+module.exports = {
+  allProducts,
+}
--- tests/unit/models/products.model.test.js
+++ tests/unit/models/products.model.test.js
@@ -1,14 +1,14 @@
 const sinon = require('sinon');
 const { expect } = require('chai');
 const connection = require('../../../src/models/connection');
-const mockProducts = require('./moks/products.model.mock');
+const mock = require('./mocks/products.model.mock');
 const productsModel = require('../../../src/models/products.model');
 
 describe('testes unitários para a camada model de produtos.', async function () {
   it('testa o retorno da função getAllProductsFromDatabase em caso de sucesso.', async function () {
     // Arrange
-    const { allProducts } = mockProducts;
-    const sucessRepostse = allProducts[0];
+    const { allProducts } = mock;
+    const sucessReponse = allProducts[0];
     sinon.stub(connection, 'execute').resolves(allProducts);
 
     // Act
@@ -22,7 +22,7 @@ describe('testes unitários para a camada model de produtos.', async function ()
 
   it('testa o retorno da função getProductByIdFromDatabase em caso de sucesso.', async function () {
     // Arrange
-    const { allProducts } = mockProducts;
+    const { allProducts } = mock;
     const sucessResponse = allProducts[0][0] // primeiro produto.
     const productID = 1;
     sinon.stub(connection, 'execute').resolves(allProducts);
