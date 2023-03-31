5cae719b7e5766a28cc9e54c6f70829d261d270f
calebeLAR <ca6ele@gmail.com>
2023-03-31 13:24:31 -0400

    req06: refat

--- src/routes/sales.router.js
+++ src/routes/sales.router.js
@@ -1,23 +1,8 @@
 const express = require('express');
-const { salesService } = require('../services');
-const { validateSaleProduct } = require('../controllers/validations/saleProductsValidation');
-const { mapError } = require('../utils/errorMap');
+const { salesController } = require('../controllers');
 
 const salesRouter = express.Router();
 
-// controller  addNewSaleProduct  
-salesRouter.post('/', async (req, res) => {
-  const saleProducts = req.body;
-
-  // validation params
-  const error = validateSaleProduct(saleProducts);
-  if (error) return res.status(mapError(error.type)).json({message: error.message});
-
-  // service
-  const {type, message } = await salesService.askToAddnewSaleProduct(saleProducts);
-  if (type) return res.status(mapError(type)).json({message});
-
-  return res.status(201).json(message)
-});
+salesRouter.post('/', salesController.addNewSaleProduct);
 
 module.exports = salesRouter;
--- src/services/sales.service.js
+++ src/services/sales.service.js
@@ -2,13 +2,39 @@ const { salesModel } = require('../models');
 const { productsModel } = require('../models');
 
 const validAllProductsExists = async (arrSales) => {
-  const arrProductsID = arrSales
+  try {
+    const arrProductsID = arrSales
     .map((sale) => productsModel.getProductByIdFromDatabase(sale.productId));
+    
+    const resolved = await Promise.allSettled(arrProductsID);
+    const thereIsAError = resolved.some((d) => d.value === undefined);
+    return thereIsAError;
 
-  const resolved = await Promise.allSettled(arrProductsID);
-  const thereIsAError = resolved.some((d) => d.value === undefined);
-  return thereIsAError;
+  } catch (dataBaseError) {
+    return { type: 'INTERNAL_ERROR', message: dataBaseError.message };
+  }
 };
+
+const insertNewSaleProcess = async (arrSales) => {
+  try {
+    const newSaleId = await salesModel.getAllSalesFromDatabase();
+    const newSaleProductId = await salesModel.insertNewSaleInTheDatabase(newSaleId.length);
+  
+    const allSalesPoductsPromisses = arrSales.map(
+      (saleProduct) => salesModel.insertNewSaleProductInTheDatabase(
+        { saleId: newSaleProductId, ...saleProduct },
+      ),
+    );
+  
+    await Promise.all(allSalesPoductsPromisses);
+
+    const { id } = await salesModel.getSaleByIdFromDatabase(newSaleProductId);
+    return id;
+
+  } catch (dataBaseError) {
+    return { type: 'INTERNAL_ERROR', message: dataBaseError.message };
+  }
+}
 
 const askToAddnewSaleProduct = async (arrSales) => {
   try {
@@ -16,23 +42,11 @@ const askToAddnewSaleProduct = async (arrSales) => {
     const error = await validAllProductsExists(arrSales);
     if (error) return { type: 'PRODUCT_NOT_FOUND', message: 'Product not found' };
 
-    const newSaleId = await salesModel.getAllSalesFromDatabase();
-    const newSaleProductId = await salesModel.insertNewSaleInTheDatabase(newSaleId.length);
+    const idNewSale = await insertNewSaleProcess(arrSales);
 
-    const allSalesPoductsPromisses = arrSales.map(
-      (saleProduct) => salesModel.insertNewSaleProductInTheDatabase(
-        { saleId: newSaleProductId, ...saleProduct },
-      ),
-    );
-
-    await Promise.all(allSalesPoductsPromisses);
-
-    const { id } = await salesModel.getSaleByIdFromDatabase(newSaleProductId);
-    // const salesProductsAdded = await salesModel.getSaleProductByIdFromDatabase(id);
-    const report = { id, itemsSold: arrSales };
-
-    return { type: null, message: report };
+    return { type: null, message: { id: idNewSale, itemsSold: arrSales } };
   } catch (dataBaseError) {
+    
     return { type: 'INTERNAL_ERROR', message: dataBaseError.message };
   }
 };
